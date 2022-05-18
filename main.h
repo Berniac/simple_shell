b7c0e0fb6859350e0289e93b71e074cea77d5fcc
BernieW <87444402+Berniac@users.noreply.github.com>
2022-05-18 10:24:28 +0300

    Create main.h

---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <limits.h>
+
+/* cmd_exec.c */
+int is_cdir(char *path, int *i);
+char *_which(char *cmd, char **_environ);
+int is_executable(data_shell *datash);
+int check_error_cmd(char *dir, data_shell *datash);
+int cmd_exec(data_shell *datash);
